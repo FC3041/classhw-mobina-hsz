130b1e71420edbe67ab7665a2d860e3c426111a0
mobina-hsz <mobinahassanzadeh099@gmail.com>
2025-02-11 00:30:09 +0330

    vance c# added till L2.4

---
+++ s01/hw/L2.3/Program.cs
@@ -0,0 +1,75 @@
+﻿// void head(int w){
+//     var k;
+//     if(w%2==0){
+//         k= w/2 + 1;}
+//     if(w%2==1){
+//         k = (w-1)/2 + 1 ;
+//     }
+//     console.Write(k * " " + "^" +k * " " );
+//      for(int i=1; i<k ; i++){
+//       console.Write(" " * (k-i)+ 
+//             i*"/" + "|" +
+//             i*"\\" + (k-i)*" ");
+//     }
+// }
+
+void head(int w ){
+    int k = (w/2) + 1;
+    for(int i=0 ; i<k ; i++){
+        Console.Write(" ");
+    }
+    Console.Write("^");
+    for(int i=0 ; i<k ; i++){
+        Console.Write(" ");
+    }
+    Console.WriteLine("");
+    // Console.Write(k*" " + "^" +k * " " );
+    for(int i=1 ; i<k ; i++){
+        for(int j=0 ; j<k-i ; j++){
+            Console.Write(" ");
+        }
+        for(int j=0 ; j<i ; j++){
+            Console.Write("/");
+        }
+        for(int j=0 ; j<i ; j++){
+            Console.Write("\\");
+        }
+        for(int j=0 ; j<k-i ; j++){
+            Console.Write(" ");
+        }
+        Console.WriteLine("");
+    //   Console.Write(" " * (k-i)+  i*"/" + "|" + i*"\\" + (k-i)*" ");
+    }
+}
+
+void line(int w){
+    Console.Write("+");
+    for(int i=0 ; i<w ; i++)
+        Console.Write("-");
+    Console.WriteLine("+");
+}
+
+void body(int h , int w){
+    for(int i=0; i<h ; i++){
+        Console.Write("+");
+        for(int j=0 ;j<w ; j++)
+            Console.Write("*");
+        Console.WriteLine("+");
+    }
+    line(w);
+}
+
+void peice(int n , int w , int h){
+    line(w);
+   for(int i=0;i<n;i++){
+      body(w,h);
+   }
+}
+
+void rocketship(int n , int w , int h){
+    head(w);
+    peice(n,w,h);
+    head(w);
+}
+
+rocketship(2,7,7);
---
+++ s01/hw/L2.4/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace L2._4;
+
+class Program
+{
+    public static void print_left_triangle(int w){
+        for(int i=0; i<w ;i++){
+            int k=i+1;
+            for(int j=0 ; j<k;j++)
+            Console.Write("*");
+            for(int j=0 ;j<w - k;j++)
+            Console.Write(" ");
+            Console.WriteLine("");
+        }
+    }
+
+    public static void print_right_triangle(int w){
+        for(int i=0; i<w ;i++){
+            int k=i+1;
+            for(int j=0 ;j<w - k;j++)
+            Console.Write(" ");
+            for(int j=0 ; j<k;j++)
+            Console.Write("*");
+            Console.WriteLine("");
+        }
+    }    
+
+    static void Main(string[] args)
+    {
+        print_left_triangle(20);
+        print_right_triangle(5);
+    }
+}
