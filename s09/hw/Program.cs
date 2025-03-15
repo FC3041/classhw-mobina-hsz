b6498bfd9aee7ce143430d2be76e844acc800cae
mobina-hsz <mobinahassanzadeh099@gmail.com>
2025-03-15 23:03:41 +0330

    hw session 9 submission

---
+++ s09/hw/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace hw;
+
+class Program
+{
+
+    static void Main(string[] args)
+    {
+        Student[] students = new Student[args.Length];
+
+        for(int i=0 ; i<args.Length ; i++){
+            students[i]= Student.Parse(args[i]); 
+        }
+
+        foreach(Student s in students){
+            File.AppendAllLines("signing.txt" , new string[]{s.ToString()});
+        }
+
+    }
+}
